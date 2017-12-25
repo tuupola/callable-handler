89f07e8e284893df2285c0c449d29e1d9e56ece8
Mika Tuupola <tuupola@appelsiini.net>
2017-12-25 19:10:46 +0700

    Add double pass __invoke() method as a trait

---
+++ src/DoublePassTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the callable handler package
+ *
+ * Copyright (c) 2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * See also:
+ *   https://github.com/tuupola/callable-handler
+ */
+
+namespace Tuupola\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+trait DoublePassTrait {
+    /**
+     * Execute as PSR-7 double pass middleware.
+     */
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        callable $next
+    ): ResponseInterface {
+        return $this->process($request, new CallableHandler($next, $response));
+    }
+}
+
