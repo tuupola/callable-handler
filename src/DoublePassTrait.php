743093aef3cea9de001d48d74aa792afbc068489
Mika Tuupola <tuupola@appelsiini.net>
2022-05-08 11:33:29 +0300

    Finish some chores for 2021 (#4)
    * Change to less flashy LICENSE
    * Hello 2021!
    * Ignore phpunit.xml.dist when installing
    * Modernize Makefile
    * Sync CONTRIBUTING.md from newer projects
    * Change to https links

--- src/DoublePassTrait.php
+++ src/DoublePassTrait.php
@@ -2,7 +2,7 @@
 
 /*
 
-Copyright (c) 2017-2018 Mika Tuupola
+Copyright (c) 2017-2021 Mika Tuupola
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@ SOFTWARE.
 
 /**
  * @see       https://github.com/tuupola/callable-handler
- * @license   http://www.opensource.org/licenses/mit-license.php
+ * @license   https://www.opensource.org/licenses/mit-license.php
  */
 
 namespace Tuupola\Middleware;
